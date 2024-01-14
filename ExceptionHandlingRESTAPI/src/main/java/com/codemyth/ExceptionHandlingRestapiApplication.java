e6cead420bdf87ae96f327e7ca6eca4e63aafe6f
CodeMyth <90126232+CodeMythGit@users.noreply.github.com>
2024-01-14 22:37:55 +0530

    Add files via upload

---
+++ ExceptionHandlingRESTAPI/src/main/java/com/codemyth/ExceptionHandlingRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.codemyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionHandlingRestapiApplication.class, args);
+    }
+
+}
